9199e2acbbe8093c606d0d5410cf6a624efc4819
dapper-module-0474 <rjwhitmercc@gmail.com>
2020-04-25 20:08:12 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,7 @@
 def find_element_index(array, value_to_find)
-  array.find(value_to_find)
+  counter = 0 
+  
+  while counter < array.length do 
+    puts array[counter] == value_to_find
+    counter += 1 
 end
